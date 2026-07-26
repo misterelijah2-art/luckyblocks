15c2bec9b8062fb48ccd2fe125dc1b4a2ddbfec4
misterelijah2-art <misterelijah2@gmail.com>
2026-07-26 12:48:55 +0200

    feat: Add entity attribute registration, loot tables for all lucky blocks and custom items

---
+++ src/main/java/luckyblocks/event/ModEvents.java
@@ -0,0 +1,11 @@
+package luckyblocks.event;
+
+import luckyblocks.entity.CursedMinerEntity;
+import luckyblocks.registry.ModEntities;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+
+public class ModEvents {
+    public static void register() {
+        FabricDefaultAttributeRegistry.register(ModEntities.CURSED_MINER, CursedMinerEntity.createAttributes());
+    }
+}
